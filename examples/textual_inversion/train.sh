f78f13e92072681d8f7fa443e9edd77f8560481c
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-06-19 17:48:56 +0900

    add train script for textual inverson task

--- examples/textual_inversion/train.sh
+++ examples/textual_inversion/train.sh
@@ -1,4 +1,4 @@
-###dreambooth.sh PSEUDO###
+###textual_inversion.sh PSEUDO###
 env_name=${1}
 model_name=${2}
 batch_size=${3}
@@ -6,7 +6,7 @@ output_dir=${4:-"outputs"}
 current_dir=$(pwd)
 log_dir=${5:-"${current_dir}/logs/${model_name}.json"}
 
-# Check if batch_size is provided , if not use default value of 1
+# Check if $batch_size is provided , if not use default value of 5
 if expr "$batch_size" + 0 > /dev/null 2>&1; then
   batch_size=$batch_size
 else
@@ -14,28 +14,28 @@ else
 fi
 
 # Check if data is downloaded
-data_dir=/nas/common_data/huggingface/dreambooth/dog
+data_dir=/nas/common_data/huggingface/textual_inversion/cat
 echo "# ========================================================= #"
 if [ -d "$data_dir" ] && [ "$(ls -A $data_dir)" ]; then
   echo "data is already in $data_dir"
 else
   echo "downloading data.."
-  conda run -n ${env_name} python3 /nas/thuchk/repos/diffusers/examples/dreambooth/download_data.py
+  conda run -n ${env_name} python3 /nas/thuchk/repos/diffusers/examples/textual_inversion/download_data.py
 fi
 
 # Run training script
 echo "# ========================================================= #"
 echo "training ${model_name}.."
-conda run -n ${env_name} python3 train_dreambooth.py \
+conda run -n ${env_name} python3 textual_inversion.py \
   --pretrained_model_name_or_path ${model_name} \
-  --instance_data_dir $data_dir \
-  --instance_prompt="a photo of sks dog" \
+  --train_data_dir $data_dir \
+  --learnable_property="object" \
+  --placeholder_token="<cat-toy>" --initializer_token="toy" \
   --resolution=512 \
   --train_batch_size ${batch_size} \
-  --gradient_accumulation_steps=1 \
-  --learning_rate=5e-6 \
+  --gradient_accumulation_steps=4 \
+  --max_train_steps=10 \
+  --learning_rate=5.0e-04 --scale_lr \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=10 \
-  --output_dir ${output_dir}  \
-  --log_dir "${log_dir}" \
+  --output_dir ${output_dir} \
