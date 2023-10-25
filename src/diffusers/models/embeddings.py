4add4d49dabd3eb4acd1a4026a998c8ad8fd0abc
odb9402 <dongpin.oh@moreh.io>
2023-10-25 14:09:34 +0900

    add efficient_unet and update others

--- src/diffusers/models/embeddings.py
+++ src/diffusers/models/embeddings.py
@@ -402,9 +402,9 @@ class AttentionPooling(nn.Module):
         super().__init__()
         self.dtype = dtype
         self.positional_embedding = nn.Parameter(torch.randn(1, embed_dim) / embed_dim**0.5)
-        self.k_proj = nn.Linear(embed_dim, embed_dim, dtype=self.dtype)
-        self.q_proj = nn.Linear(embed_dim, embed_dim, dtype=self.dtype)
-        self.v_proj = nn.Linear(embed_dim, embed_dim, dtype=self.dtype)
+        self.k_proj = nn.Linear(embed_dim, embed_dim)
+        self.q_proj = nn.Linear(embed_dim, embed_dim)
+        self.v_proj = nn.Linear(embed_dim, embed_dim)
         self.num_heads = num_heads
         self.dim_per_head = embed_dim // self.num_heads
 
